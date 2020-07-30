2281a84a82c54491682ad17eb5ce7798e394f41e
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2020-07-29 21:42:39 -0300

    Implementando conversão em imagem binária

---
+++ filtrosDeTransformacao/colorida/Binaria.py
@@ -0,0 +1,72 @@
+# *-* coding: utf:8 -*-
+
+import sys
+import numpy as np
+
+
+def receberArquivos():
+    # Abrir os arquivos de entrada e de saída
+    entrada = open(sys.argv[1], "r+")
+    saida = open(sys.argv[2], "w+")
+
+    linha = entrada.readline() # Tipo
+    linha = entrada.readline() # Comentário
+    linha = entrada.readline() # Dimensões
+    dimensoes = linha.split()
+    largura = int(dimensoes[0])
+    altura = int(dimensoes[1])
+    linha = entrada.readline() # Valor fixo
+    linha = entrada.readlines()
+
+    #converter de lista para array
+    imagem = np.asarray(linha, dtype=int)
+    imagem = np.reshape(imagem, (altura, largura, 3))
+    linhas = converterParaEscalaDeCinza(imagem)
+    # converter de lista para array
+    imagem = np.asarray(linhas, dtype=int)
+    imagem = np.reshape(imagem, (altura, largura))
+
+    gerarImagemTransformada(entrada, saida, largura, altura, imagem)
+
+def converterParaEscalaDeCinza(imagem):
+    linhas = []
+
+    #converter pixels coloridos para escala de cinza
+    for i in range(len(imagem)):
+        for j in range(len(imagem[1])):
+            sum = 0
+            for k in range(3):
+                sum = sum + imagem[i][j][k]
+            sum = int(sum / 3)
+            sum = str(sum)
+            linhas.append(sum)
+    return linhas
+
+
+def gerarImagemTransformada(entrada, saida, largura, altura, imagem):
+
+    #escrevendo a imagem cópia
+    saida.write("P1\n")
+    saida.write("#Criado por Thais\n")
+    saida.write(str(largura))
+    saida.write(" ")
+    saida.write(str(altura))
+    saida.write("\n")
+    limite = 192
+
+    #fazer a cópia para imagem binária
+    for i in range(len(imagem)):
+        for j in range(len(imagem[1])):
+            if imagem[i][j] > limite:
+                saida.write("0")
+            else:
+                saida.write("1")
+            saida.write("\n")
+
+    #fechar os dois arquivos.
+    entrada.close()
+    saida.close()
+
+
+if __name__ == "__main__":
+    receberArquivos()
