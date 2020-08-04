2812f501171c1db8f2036b13cd2baa7e9157e0fa
Thais Vilarinho <thais.apvilarinho@hotmail.com>
2020-08-04 12:23:32 -0300

    Refatorando código

--- filtrosDeTransformacao/pretoBranco/DeteccaoDeBordasCrono.py
+++ filtrosDeTransformacao/pretoBranco/DeteccaoDeBordasCrono.py
@@ -43,14 +43,14 @@ def escreverImagemSaida(entrada, largura, altura, imagem, kernel, ks):
     saida = open(sys.argv[2], "w+")
     saida.write("P1\n")
     saida.write("#Criado por Thais\n")
-    saida.write(str(largura-(ks*2)))
+    saida.write(str(largura - (ks * 2)))
     saida.write(" ")
-    saida.write(str(altura-(ks*2)))
+    saida.write(str(altura - (ks * 2)))
     saida.write("\n")
 
     # aplicação detecção de bordas
-    for i in range(ks, len(imagem)-ks):
-        for j in range(ks, len(imagem[1])-ks):
+    for i in range(ks, len(imagem) - ks):
+        for j in range(ks, len(imagem[1]) - ks):
             sum = 0
             for ki in range(len(kernel)):
                 for kj in range(len(kernel[1])):
@@ -63,11 +63,9 @@ def escreverImagemSaida(entrada, largura, altura, imagem, kernel, ks):
             saida.write(sum)
             saida.write("\n")
 
-
     # fechar os arquivos
     entrada.close()
     saida.close()
-
 
 
 if __name__ == "__main__":
